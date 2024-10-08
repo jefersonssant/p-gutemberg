11869205eeda92fd2b7b76387e918d4e18b8af70
jefersonssant <jefss@outlook.com>
2024-10-08 20:08:38 -0300

    Alterações no index para inclusão de novo advogado e alterações no eventos.

--- assets/js/eventsSlides.js
+++ assets/js/eventsSlides.js
@@ -1,16 +1,25 @@
-let count = 1;
-document.getElementById("radio1").checked = true;
+let count_1 = 1;
+let count_2 = 1;
+const totalSlides1 = 5;
+const totalSlides2 = 5
+document.getElementById("radio1-1").checked = true;
+document.getElementById("radio2-1").checked = true;
 
 setInterval(function() {
   nextImage()
 }, 3000)
 
 function nextImage() {
-  count++;
-  if (count > 5) {
-    count = 1;
+
+  document.getElementById("radio1-"+count_1).checked = true;
+  count_1++;
+  if (count_1 > totalSlides1) {
+    count_1 = 1;
   }
 
-  document.getElementById("radio"+count).checked = true;
-
+  document.getElementById("radio2-"+count_2).checked = true;
+  count_2++;
+  if (count_2 > totalSlides2) {
+    count_2 = 1;
+  }
 }
